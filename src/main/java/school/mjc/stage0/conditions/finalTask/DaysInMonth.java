66844495600ba9ae425a2ade52219f4f52095c9e
Philip <okruzhko.f@gmail.com>
2023-10-09 18:43:25 +0400

    Complete s0m4Final (without exercise 6)

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,43 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if (isLeapYear(year) && month == 2) {
+            System.out.println(29);
+        } else {
+            amountOfDays(month);
+        }
+    }
+
+    private boolean isLeapYear(int year) {
+        if (
+                (year % 400 == 0 && year % 100 == 0) ||
+                        (year % 4 == 0 && year % 100 != 0)
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void amountOfDays(int month) {
+        if (month < 1 || month > 12) {
+            System.out.println("wrong number!");
+        }
+
+        if (month == 2) {
+            System.out.println(28);
+        }
+
+        if (month == 2 && isLeapYear();)
+
+        if (((month >= 1 && month <= 7) && month % 2 != 0) ||
+                (month >= 8 && month <= 12) && month % 2 == 0) {
+            System.out.println(31);
+        }
+
+        if ((month >= 4 && month <= 6) && month % 2 == 0) {
+            System.out.println(30);
+        }
 
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        int result = dividend / divider;
+        if (divider == 0) {
+            System.out.println("division by zero");
+        } else if (result * divider == divider) {
+            System.out.println("can be divided completely");
+        } else {
+            System.out.println("cannot be divided completely");
+        }
     }
 }
